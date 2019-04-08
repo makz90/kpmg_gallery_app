efeb9763d2fb6d9d65c7eea0031770178b5b8000
Maksym Krawczyk <TNCJ68@motorolasolutions.com>
2019-04-08 10:55:19 +0200

    Adds custom control with droid renderer

---
+++ GalleryApp.Android/Renderers/DroidEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using GalleryApp.Droid.Renderers;
+using GalleryApp.Pages.Base;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AllCapsEntry), typeof(DroidEntryRenderer))]
+namespace GalleryApp.Droid.Renderers
+{
+    public class DroidEntryRenderer : EntryRenderer
+    {
+        public DroidEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Element is AllCapsEntry myEntry && myEntry.HasAllCapitals)
+            {
+                Control.Text = Element.Text.ToUpper();
+            }
+        }
+    }
+}
---
+++ GalleryApp/Pages/Base/AllCapsEntry.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace GalleryApp.Pages.Base
+{
+    public class AllCapsEntry : Entry
+    {
+        public static readonly BindableProperty HasAllCapitalsProperty = BindableProperty.Create(nameof(HasAllCapitals),
+            typeof(bool),
+            typeof(AllCapsEntry),
+            (object)false,
+            BindingMode.OneWay);
+
+        public bool HasAllCapitals
+        {
+            get
+            {
+                return (bool)this.GetValue(HasAllCapitalsProperty);
+            }
+            set
+            {
+                this.SetValue(HasAllCapitalsProperty, (object)value);
+            }
+        }
+    }
+}
